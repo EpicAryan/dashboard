afc97757376a36d4ca2e0f647f89b38fc0ad19cd
EpicAryan <adaryan0811@gmail.com>
2025-07-24 07:52:33 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+function App() {
+
+  return (
+    <>
+      <h1 className="App text-3xl font-bold underline">
+        Hello world!
+      </h1>    
+    </>
+  )
+}
+
+export default App
